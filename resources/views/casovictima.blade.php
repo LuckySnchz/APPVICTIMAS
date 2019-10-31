064aaff11e18fc08d94a6c5856dfb3d14bc14672
LuckySnchz <calder07@gmail.com>
2019-10-31 16:09:06 -0300

    new view

---
+++ resources/views/casovictima.blade.php
@@ -0,0 +1,65 @@
+<?php
+
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+         <link href="/css/styles.css" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+      <link rel="stylesheet" href="css/app.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+      <title>App Víctimas</title>
+      <style>
+   .btn{margin-left: -3%}
+
+
+      </style>
+       <a name="Ancla" id="Ancla"></a>
+   </head>
+  <header>
+      <section class=" container jumbotron shadow p-3 mb-5 bg-white rounded"style="height: 150px" >
+     @include('navbar')
+
+                <a type="button"  href="/home" target="_self" style="width:100%; color:white;background-color:rgb(52, 144, 220);margin-bottom: -5%;margin-top: -12%;margin-left: 0.1%" class="btn col-XL" class="btn btn-danger">IR A INICIO</button> </a><br><br>
+      </section>        
+      
+       <section class="container jumbotron shadow p-3 mb-5 bg-white rounded"style="height: 180px" >
+    
+<br><br>
+                <a type="button"  href="/paneldecontrolvictima/{{$caso->id}}" target="_self" style="width:100%; color:white;background-color:rgb(137, 210, 14);margin-bottom: -5%;margin-top: -8%;margin-left: 0.1%;color: black" class="btn col-XL" class="btn btn-danger">IR A VICTIMA</button> </a><br><br>
+
+                 <a type="button"  href="/paneldecontrolcaso/{{$caso->id}}" target="_self" style="width:100%; color:white;background-color:rgb(52, 144, 220);margin-bottom: -1%;margin-top: -1%;margin-left: 0.1%;color: black" class="btn col-XL" class="btn btn-danger">IR A CASO</button> </a><br><br><br><br>
+
+      </section> 
+
+   </header>
+
+
+
+
+
+
+
+
+   <body>
+
+
+
+
+
+
+
+
+
+
+   </body>
+</html>
