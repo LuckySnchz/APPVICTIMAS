673583799e81a395b9405a98fcf9a61f8e5f6fd3
LuckySnchz <calder07@gmail.com>
2019-11-04 12:55:21 -0300

    panel de control victima

--- resources/views/paneldecontrolcaso.blade.php
+++ resources/views/paneldecontrolcaso.blade.php
@@ -61,40 +61,7 @@ session_start();
          
 
 
- <!-- <div class="dropdown">
-    <button type="button"  style="background-color:white;position: fixed;display: inline-block;margin-top: -3.43%;margin-left: 0.1%;width: 23%"data-toggle="dropdown">
-     Menu
-    </button>
-
-    <div class="dropdown-menu">
-     
-    
-     <ul>
-         <li class="nav-item"> <strong><a class="nav-link " style="color:black;font-size:1.1em" href="#INICIO">Seleccionar Víctima</a> </li></strong>
-        
-
-
-
-            <li class="nav-item"><strong> <a class="nav-link "  style="color:black;font-size:1.1em" href="#A">Institución/Personas Asistidas</a> </li></strong>
-
-         <li class="nav-item"><strong> <a class="nav-link "  style="color:black;font-size:1.1em" href="#AA">Profesionales Intervinientes</a> </li></strong>
-
-        <li class="nav-item"><strong> <a class="nav-link " style="color:black;font-size:1.1em" href="#B">Víctima y su contexto</a> </li></strong>      
-       
-         <li class="nav-item"><strong> <a class="nav-link " style="color:black;font-size:1.1em"  href="#C">Referentes Afectivos</a> </li></strong>
-
-         <li class="nav-item"><strong> <a class="nav-link " style="color:black;font-size:1.1em" href="#D">Imputado</a> </li></strong>
-
-         <li class="nav-item"><strong> <a class="nav-link " style="color:black;font-size:1.1em" href="#E">Atención del Caso</a> </li></strong>
-
-         <li class="nav-item"><strong> <a class="nav-link "style="color:black;font-size:1.1em"  href="#FIN">Documentación</a> </li></strong>
-
-          <li class="nav-item"><strong> <a class="nav-link "style="color:black;font-size:1.1em"  href="/home">Ir a HOME</a> </li></strong>
-      </ul>
-    
-    </div>
-
-  </div>-->
+ 
 
    <br><br>
 
