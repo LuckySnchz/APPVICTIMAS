4de9399f95e26743dbff04b3d61d848f7c160485
LuckySnchz <calder07@gmail.com>
2019-11-26 08:19:35 -0300

    json Api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -9,10 +9,8 @@ class ApiController extends Controller
 {
     
     public function getdatos(){
-        //$datos=Victim::all();
-        $casos = Caso::
-    join('casos', 'casos.id', '=', 'victims.idCaso')
-    ->select('victims.victima_nombre_y_apellido','victims.tipodocumento','victims.victima_numero_documento','victims.victima_fecha_nacimiento','casos.cavaj','casos.fecha_ingreso')
+     
+        $datos=DB::table('casos') ->join('victims', 'casos.id', '=', 'victims.idCaso') ->select('victims.victima_nombre_y_apellido','victims.tipodocumento','victims.victima_numero_documento','victims.victima_fecha_nacimiento','casos.cavaj','casos.fecha_ingreso')
        ->get();
         return response()->json($datos);
 }
